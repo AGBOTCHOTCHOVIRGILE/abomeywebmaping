b2a3bc978303b56fa4fe89535978e2eb4b6cec90
AGBOTCHOTCHOVIRGILE <129605117+AGBOTCHOTCHOVIRGILE@users.noreply.github.com>
2023-04-01 19:48:54 +0100

    initial upload file

---
+++ styles/cours_deau_3_style.js
@@ -0,0 +1,64 @@
+var size = 0;
+var placement = 'point';
+function categories_cours_deau_3(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'CANAL_IRRIGATION':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(36,39,232,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'FLEUVE':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(36,39,232,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'RIVIERE':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(36,39,232,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'RUISSEAU':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(36,39,232,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_cours_deau_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("type");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_cours_deau_3(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
